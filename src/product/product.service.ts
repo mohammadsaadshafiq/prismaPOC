c7638620aad892d383ed333fe3281ea4b7d23e59
Saad Shafiq <>
2022-11-05 20:56:56 +0100

    service code

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { Product } from '../product'; 
 import { PrismaClient, Prisma } from '@prisma/client'
 import { map } from 'rxjs';
@@ -13,17 +13,36 @@ export class ProductService {
     return result;
   }
   async findAll(){
-   const result= await this.prisma.product.findMany();
-   console.log("saad",result);
-   return result;
+    try {
+      await this.prisma.product.findMany()
+    } catch (e) {
+      if (e instanceof Prisma.PrismaClientKnownRequestError) {
+        // The .code property can be accessed in a type-safe manner
+        if (e.code === 'P2002') {
+          console.log(
+            'There is a unique constraint violation, a new user cannot be created with this email'
+          )
+        }
+      }
+      throw e
+    }
   }
   async findOne(productId: string) {
-    const result = await this.prisma.product.findUnique({ where:{id:productId} });
-    return result;
+    const  result = await this.prisma.product.findUnique({ where:{id:productId} }); 
+    console.log(result)
+    if (result){
+      return result
+    }
+    else {
+      throw new HttpException('Forbidden', HttpStatus.FORBIDDEN);
+    }
   }
   async remove(productId: string) {
-     const result = await this.prisma.product.delete({ where:{id:productId}  });
-     return "Product Deleted";
+     const result = await this.prisma.product.delete({
+      where: {
+        id: productId,
+      },
+    })
   }
   async update(productId: string, updatedProduct: Product) {
     const result = await this.prisma.product.update({
